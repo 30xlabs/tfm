f767c015d44710b1c4e316934274c576e3660aff
Saketh Kowtha <saketh@30xlabs.com>
2023-12-12 15:59:51 +0530

    added log

--- src/api/slack-events.js
+++ src/api/slack-events.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
   if (req.method !== `POST`) return res.status(405).send("Invalid HTTP method")
   const body = req.body
+  console.log(req.body)
   res.status(200).json({ hello: `world`, body })
 }
