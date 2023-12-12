432b7b37d7045d5a80dab6fb0493819b99d005b0
Saketh Kowtha <saketh@30xlabs.com>
2023-12-12 16:25:54 +0530

    Refactored log

--- src/api/slack-events.js
+++ src/api/slack-events.js
@@ -1,6 +1,22 @@
+const notifySlack = json => {
+  return fetch(
+    "https://hooks.slack.com/services/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",
+    {
+      method: "POST",
+      body: new URLSearchParams({
+        payload: `{"channel": "#tfm-build", "username": "${JSON.stringify(
+          json,
+        )}", "icon_emoji": ":ghost:"}`,
+      }),
+    },
+  )
+}
 export default function handler(req, res) {
   if (req.method !== `POST`) return res.status(405).send("Invalid HTTP method")
   const body = req.body
-  console.log(req.body)
-  res.status(200).json({ hello: `world`, body })
+  notifySlack(body)
+    .then(() => {
+      res.status(200).send("Success")
+    })
+    .catch(() => res.status(500).send("Internal server error"))
 }
